5414f144a457433fc085f5c619157c8766a588fe
Thomas Clukies <thomas.clukies@gmail.com>
2018-10-26 11:38:43 -0600

    getting token in route

--- app.js
+++ app.js
@@ -19,24 +19,16 @@ function giveMeAToken(){
           grant_type: "client_credentials"
       },
       rejectUnauthorized: false
-    
     };
-    let json   
-    return request(options).then((error, response) => {
-    // if (error) throw new Error(error);
-        // console.log(error)
-        // console.log(body)
-        json = JSON.parse(response)
-        console.log("1" + json.access_token)
-        return json.access_token
+    return request(options, function (error) {
+      if (error) throw new Error(error);
     })
-
+    .then(res => JSON.parse(res).access_token)
 }
 
-app.get("/", (req, res) => {
-    let token = giveMeAToken()
-    console.log("3" + token)
-    //hard code client credentials to get auth token
+app.get("/", async (req, res) => {
+    let token = await giveMeAToken()
+    console.log("2" + token)
     //hit the oauth route
 
 
