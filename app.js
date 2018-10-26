01da57763c5515670bf537174d312c764e3ee9e7
Thomas Clukies <thomas.clukies@gmail.com>
2018-10-26 12:05:41 -0600

    working: composite users route getting a users devices by id

--- app.js
+++ app.js
@@ -1,46 +1,54 @@
-const express = require("express")
-const app = express()
-const port = 8080
+const express = require("express");
+const app = express();
+const port = 8080;
 const request = require("request-promise");
 // const bodyParser = require("body-parser");
 
 // app.use(bodyParser.json());
 
-function giveMeAToken(){
-    var options = { method: 'GET',
-      url: 'https://auth.qa.fitpay.ninja/oauth/token',
-      auth: {
-          //pass this as env variable
-          user: "plFYHKtt",
-          pass: "5AditH9m"
-      },
-      //query string option
-      qs: {
-          grant_type: "client_credentials"
-      },
-      rejectUnauthorized: false
+function giveMeAToken() {
+    var options = {
+        method: "GET",
+        url: "https://auth.qa.fitpay.ninja/oauth/token",
+        auth: {
+            //pass this as env variable
+            user: "plFYHKtt",
+            pass: "5AditH9m"
+        },
+        //query string option
+        qs: {
+            grant_type: "client_credentials"
+        },
+        rejectUnauthorized: false
     };
-    return request(options, function (error) {
-      if (error) throw new Error(error);
-    })
-    .then(res => JSON.parse(res).access_token)
+    return request(options, function(error) {
+        if (error) throw new Error(error);
+    }).then(res => JSON.parse(res).access_token);
 }
 
-app.get("/", async (req, res) => {
-    let token = await giveMeAToken()
-    console.log("2" + token)
+app.get("/compositeUsers/:userId", async (req, res) => {
+    let token = await giveMeAToken();
+    // console.log("2" + token);
     //hit the oauth route
+    var options = {
+        method: "GET",
+        url: "https://api.qa.fitpay.ninja/users/"+ req.params.userId + "/devices",
+        headers: {
+            Authorization: "Bearer " + token
+        },
+        rejectUnauthorized: false
 
+    };
 
+    request(options, function(error, response, body) {
+        if (error) throw new Error(error);
 
-    
+        console.log(body);
+    });
+
     //hit all three of routes with that oauth token
     //combine into one json
-
-
-    
-})
-
+});
 
 app.listen(port, () => {
     console.log("listening on port", port);
